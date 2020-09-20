381978b283f46b322e826e74f5c3c57d17dcc97b
Tunnelvision77 <Tacomablues666@gmail.com>
2020-09-20 16:33:05 -0600

    after build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  outputDir: "../client",
-  productionSourceMap: false
+  outputDir: "docs",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/checkpoint-vicecity-blog/" : "/",
 };
